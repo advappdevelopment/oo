dd44cc5a551454dc5ded8619b89787dd22799901
advappdevelopment <home-pc@Unknown-0c-4d-e9-99-ff-64.home>
2015-02-02 12:46:28 +0000

    Initial Commit

---
+++ oo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oo
+//
+//  Created by home on 02/02/2015.
+//  Copyright (c) 2015 ADV App Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ oo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  oo
+//
+//  Created by home on 02/02/2015.
+//  Copyright (c) 2015 ADV App Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
